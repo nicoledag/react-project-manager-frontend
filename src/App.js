7745b4f23570641b439cc61f411d0129c7ff757e
Nicole Dagnenica <nnardella@gmail.com>
2020-04-09 13:13:54 -0400

    Create redux store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {Route, Switch } from 'react-router-dom' 
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 class App extends Component {
 
@@ -9,8 +12,14 @@ class App extends Component {
   }
 
   render() { 
-    return ( <div>
+    return ( 
+      
+    <div>
+      <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/signup' component={Register}/>
 
+      </Switch>
     </div> );
   }
 }
