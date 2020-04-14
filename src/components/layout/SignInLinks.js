3fafb70da7de4685250b9ecc91652a6473f10674
Nicole Dagnenica <nnardella@gmail.com>
2020-04-14 14:15:10 -0400

    Update NavLinks on navigation bar

--- src/components/layout/SignInLinks.js
+++ src/components/layout/SignInLinks.js
@@ -13,24 +13,23 @@ import { withRouter } from 'react-router'
 
             return (
                 <nav className="my-navbar">
-                   <li><NavLink to="/" className="my-nav-links products-desktop">Product Categories</NavLink></li> 
+                   <li><NavLink to="/" className="my-nav-links products-desktop">Project Dashboard</NavLink></li> 
 
                     <ul className="main-nav" id="js-menu">
                     <div className="dropdown">
                             <button className="dropbtn">+<FontAwesomeIcon icon={faCaretDown}/></button>
                             <div className="dropdown-content">
-                                <li><NavLink to="/" className="my-nav-links products-mobile">Product Categories</NavLink></li> 
-                                <li><NavLink to="/businesses/new" className="my-nav-links">New Project</NavLink></li>
-                                <li><NavLink to="/businesses/new" className="my-nav-links">New Client</NavLink></li>
+                                <li><NavLink to="/projects/new" className="my-nav-links">New Project</NavLink></li>
+                                <li><NavLink to="/clients/new" className="my-nav-links">New Client</NavLink></li>
                             </div>
                         </div>
 
                         <div className="dropdown">
                             <button className="dropbtn">hello {props.currentUser.attributes.username}! <FontAwesomeIcon icon={faCaretDown}/></button>
                             <div className="dropdown-content">
-                            <li><NavLink to="/businesses/new" className="my-nav-links">My Projects</NavLink></li>
+                            <li><NavLink to="/" className="my-nav-links products-mobile">Product Dashboard</NavLink></li>                            
+                            <li><NavLink to="/projects" className="my-nav-links">Open Projects</NavLink></li>
                                 <li><NavLink to="/clients" className="my-nav-links">Clients</NavLink></li> 
-                                <li><NavLink to="/" className="my-nav-links products-mobile">Product Categories</NavLink></li>                            
                                 <li><button onClick={props.logOut}  className="nav-button">Logout</button></li>
                             </div>
                         </div>
