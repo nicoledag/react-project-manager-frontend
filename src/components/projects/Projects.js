77fa9873a55b1e540f12e9c17c89c254cdc14204
Nicole Dagnenica <nnardella@gmail.com>
2020-04-17 15:51:13 -0400

    Create projects component and pass in props

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Projects = (props) => {
+
+        console.log("projects props", props)
+        return ( 
+            <div>Projects are here</div>
+         );
+    }
+ 
+        
+
+ 
+export default Projects;
+
--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { fetchProjects } from "../actions/project.js"
+import Projects from '../components/projects/Projects.js'
 
 class DashboardContainer extends Component {
     state = {  }
@@ -10,12 +11,21 @@ class DashboardContainer extends Component {
     }
 
     render() { 
+        console.log(this.props)
         return ( 
             <div>
                 <h1>Projects</h1>
+                
+                <Projects projectsList={this.props}/>
             </div> 
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        projects: state.projectReducer
+    }
+}
  
-export default connect(null, { fetchProjects })(DashboardContainer);
+export default connect(mapStateToProps, { fetchProjects })(DashboardContainer);
