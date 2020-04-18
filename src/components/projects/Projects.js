95fe79f3f342b6298d635d1dac52d1d7c682a6f0
Nicole Dagnenica <nnardella@gmail.com>
2020-04-18 18:12:01 -0400

    Add mapStateToProps to redirect if not logged in

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 const Projects = (props) => {
+
+    const { loggedIn } = props;
+    // console.log("loggedIn", loggedIn)
+    if (!loggedIn) return <Redirect to='/' />
 
         console.log("projects props", props)
         return ( 
@@ -9,7 +15,12 @@ const Projects = (props) => {
     }
  
         
-
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser,
+        loggedIn: !!currentUser
+    }
+}
  
-export default Projects;
+export default connect(mapStateToProps)(Projects);
 
