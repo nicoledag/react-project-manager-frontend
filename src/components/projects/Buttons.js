77f8601bd7b26c98b691b33c2cfe5bd5214fb95d
Nicole Dagnenica <nnardella@gmail.com>
2020-04-18 23:29:28 -0400

    Add buttons component

---
+++ src/components/projects/Buttons.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Buttons extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Button variant="contained">Default</Button>
+                <Button variant="contained" color="primary">
+                Primary
+                </Button>
+                <Button variant="contained" color="secondary">
+                Secondary
+                </Button>
+                <Button variant="contained" disabled>
+                Disabled
+                </Button>
+                {/* <Button variant="contained" color="primary" href="#contained-buttons">
+                Link
+                </Button> */}
+        </div>
+          );
+    }
+}
+ 
+export default Buttons;
--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import { fetchProjects } from "../actions/project.js"
 import Projects from '../components/projects/Projects.js'
-import { Button } from '@material-ui/core';
+
+import Buttons from '../components/projects/Buttons.js'
 
 
 class DashboardContainer extends Component {
@@ -15,22 +16,12 @@ class DashboardContainer extends Component {
     render() { 
         console.log(this.props)
         return ( 
-            <div>
-                <h2>Welcome</h2>
-                <Button variant="contained">Default</Button>
-                <Button variant="contained" color="primary">
-                Primary
-                </Button>
-                <Button variant="contained" color="secondary">
-                Secondary
-                </Button>
-                <Button variant="contained" disabled>
-                Disabled
-                </Button>
-                {/* <Button variant="contained" color="primary" href="#contained-buttons">
-                Link
-                </Button> */}
-                {/* <Projects projectsList={this.props}/> */}
+            <div className="data-container">
+                <div>
+                    <h2>Welcome</h2>
+                    <Buttons />
+                    <Projects projectsList={this.props}/>
+                </div>
             </div> 
         );
     }
