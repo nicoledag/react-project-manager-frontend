e6275728208c72b9d0ccfca637db29c81ea296b3
Nicole Dagnenica <nnardella@gmail.com>
2020-04-19 09:32:39 -0400

    Create allProjects component and import buttons

---
+++ src/components/projects/OpenProjects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Buttons from './Buttons'
+
+const OpenProjects = () => {
+
+    return(
+        <div>
+            <Buttons />
+            <h1>Open Projects</h1>
+             
+        </div>
+    )
+
+}
+
+export default OpenProjects
