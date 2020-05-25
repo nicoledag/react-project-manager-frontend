a3bf6f064fe50825aa262844bbcd2a333453f73e
Nicole Dagnenica <nnardella@gmail.com>
2020-05-24 21:59:32 -0400

    Add client key

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 
 const Clients = (props) => {
 
     const { loggedIn } = props;
     const clientList = props.clientList.clients.clients.map(client => 
-         <li>{client.attributes.name}</li>
+        <div key={client.id} >
+         <li><Link to={`/clients/${client.id}`}>{client.attributes.name}</Link></li>
+         </div>
     )
 
     console.log(clientList)
